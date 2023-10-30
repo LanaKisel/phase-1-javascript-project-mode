d57250fdcc634c84a658d549520476c73a5ccef0
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-10-30 15:30:35 -0700

    creates "exit button"

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function addsComment(name, comment) {
     document.querySelector(".nameHolder").appendChild(feedbackName);
     document.querySelector("#feedback").reset();
 }
-
 function getAllCommentsForArtWorkById(artworkId) {
     document.querySelector(".holdFeedback").innerHTML = "";
     document.querySelector(".nameHolder").innerHTML = "";
@@ -20,18 +19,19 @@ function getAllCommentsForArtWorkById(artworkId) {
     })
         .then(res => res.json())
         .then(data => {
-            
+
             let filteredComments = data.filter((comments) => {
                 return comments.artworkId === artworkId
             })
-            filteredComments=filteredComments.splice(-5)
+            filteredComments = filteredComments.splice(-5)
 
             for (element of filteredComments) {
 
                 addsComment(element.name, element.comment)
-            } 
+            }
         })
 }
+
 document.addEventListener("DOMContentLoaded", () => {
     const art = document.querySelector("#artDescription");
     art.addEventListener('mouseover', (event) => {
@@ -46,11 +46,11 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener('submit', function (event) {
         event.preventDefault();
         let name = document.querySelector(".text").value;
-        let comment =document.querySelector(".input-text").value;
+        let comment = document.querySelector(".input-text").value;
         console.log("name length", name.length);
-        if (name.length==0){
-            name="anonymous"
-        };        
+        if (name.length == 0) {
+            name = "anonymous"
+        };
         fetch('http://localhost:3000/comments', {
             method: "POST",
             headers: {
@@ -72,6 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let curIndex = 0;
     const beginButton = document.querySelector("#begin").addEventListener('click', () => {
         curIndex = 0;
+        
         fetch('http://localhost:3000/artWork', {
             method: "GET",
             headers: {
@@ -79,11 +80,13 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         })
             .then(res => res.json())
-            .then(data => {         
+            .then(data => {
+            
                 let btn1 = document.getElementById("btn1");
                 let btn2 = document.getElementById('btn2');
                 let btn3 = document.getElementById("btn3");
-                console.log(curIndex);              
+                console.log(curIndex);
+                
                 if (!btn1) {
                     btn1 = document.createElement('button');
                     btn1.textContent = "<< Previous gallery item";
@@ -91,63 +94,64 @@ document.addEventListener("DOMContentLoaded", () => {
                     btn1.addEventListener("click", function (e) {
                         console.log(curIndex)
                         curIndex--;
+                        indexUpdated(data.length)
                         if (curIndex < 0) {
                             document.querySelector("#gallery-header").classList.remove("hidden");
                             document.querySelector("#page_wrapper").classList.add("hidden")
                         } else if (curIndex == 0) {
                             this.textContent = "<< Exit gallery";
-                            updateDisplay(curIndex);                          
-                           } else {
-                            btn2.textContent="Next gallery item >>";
                             updateDisplay(curIndex);
-                           }
-                       // updateDisplay(curIndex);
+                        } else {
+                            btn2.textContent = "Next gallery item >>";
+                            updateDisplay(curIndex);
+                        }
                     });
                 }
                 if (!btn2) {
                     btn2 = document.createElement('button');
-                    btn2.id='btn2';
-                    btn2.textContent = "Next gallery item >>";                    
+                    btn2.id = 'btn2';
+                    btn2.textContent = "Next gallery item >>";
                     btn2.addEventListener('click', function (e) {
                         curIndex++;
-                        if (curIndex > data.length-1) {
+                        indexUpdated(data.length);
+
+                        if (curIndex > data.length - 1) {
                             document.querySelector("#gallery-header").classList.remove("hidden");
                             document.querySelector("#page_wrapper").classList.add("hidden");
-                        } else if (curIndex == data.length-1) {
+                        } else if (curIndex == data.length - 1) {
                             this.textContent = "Exit gallery  >>";
                             updateDisplay(curIndex);
                         } else {
-                            btn1.textContent="<< Previous gallery item";
+                            btn1.textContent = "<< Previous gallery item";
                             updateDisplay(curIndex);
                         }
-                       // updateDisplay(curIndex);
                     });
                 }
-                if (!btn3) {
-                    // if (curIndex>0 || curIndex < data.length-1) {
-                    btn3=document.createElement('button');
-                    btn3.id='btn3';
-                    btn3.textContent = 'Exit gallery';
-                    if (curIndex>0 || curIndex < data.length-1) {
-                    document.querySelector("#artWork").appendChild(btn3)
-                    btn3.addEventListener('click', function(e) {
-                        document.querySelector("#gallery-header").classList.remove("hidden");
-                        document.querySelector("#page_wrapper").classList.add("hidden");})
-     }
-                updateDisplay(curIndex);
-            }                    
-                
-                btn2.textContent="Next gallery item >>"
-                btn1.textContent="Exit gallery"
+                if (!btn3) {                    
+                        btn3 = document.createElement('button');
+                        btn3.id = 'btn3';
+                        btn3.textContent = 'Exit gallery';
+                        btn3.addEventListener('click', function (e) {
+                            indexUpdated(data.length);
+                            document.querySelector("#gallery-header").classList.remove("hidden");
+                            document.querySelector("#page_wrapper").classList.add("hidden");
+                            updateDisplay(curIndex);
+
+                        })
+                    
+                }                    
+                btn2.textContent = "Next gallery item >>"
+                btn1.textContent = "Exit gallery"
                 document.querySelector("#gallery-header").classList.add("hidden");
                 document.querySelector("#page_wrapper").classList.remove("hidden");
                 updateDisplay(curIndex);
+                indexUpdated(data.length);
                 function updateDisplay(index) {
-                    
+
                     console.log("data", data);
                     console.log("index", index);
                     console.log(data[index].id);
-            
+
                     currentArtworkId = data[index].id;
                     document.querySelector("#artDescription").innerHTML = ""
                     let currentArtPiece = data[index]
@@ -168,13 +172,30 @@ document.addEventListener("DOMContentLoaded", () => {
                     a.textContent = `Wikipedia article: ` + currentArtPiece.title;
                     console.log(a)
                     art.appendChild(a);
+                    artWork.appendChild(btn3);
                     artWork.appendChild(btn1);
                     artWork.appendChild(btn2);
-                    //artWork.appendChild(btn3);
-
                     getAllCommentsForArtWorkById(currentArtworkId);
                     document.querySelector("#artDescription").childNodes.remove;
                 }
             })
-    })
+   })
+
+    const indexUpdated = function (galleryItems) {
+    
+        console.log("Index Value: ", curIndex);
+        if (curIndex <= 0 && galleryItems == 1){
+            btn3.classList.add('hidden')
+            console.log("I'm on the ONLY gallery item!" );
+        }else if (curIndex <= 0){
+            btn3.classList.add('hidden')
+            console.log("I'm on the first gallery item!" );
+        }else if(curIndex == galleryItems - 1){
+            btn3.classList.add('hidden')
+            console.log("I'm on the last gallery item!" ); 
+        }else {
+            btn3.classList.remove('hidden')
+            console.log("I'm somewhere in-between gallery items!" ); 
+        }
+    }
 })
